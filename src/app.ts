e89f0cd1a9d56ed82aff611cc72285085d31a76b
shinkouu <pham_kevin@outlook.de>
2025-04-03 20:16:30 +0200

    first init

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+/*
+    Title: Tic Tac Toe
+    Brief description of the program: Browser Tic Tac Toe
+
+    Name of the author: Kevin Pham
+    Date of creation 03.04.2025
+    Datum of last edit: 03.04.2025
+*/
+
+let isPlayer0: boolean = true;
+
+const winPatterns: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
